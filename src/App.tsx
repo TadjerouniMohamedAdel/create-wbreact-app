ffc29cc1a354141f618f36cccdd08393a9a117f1
TadjerounMohamedAdel <mohnagato@gmail.com>
2022-03-09 12:23:37 +0100

    unused styles support

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles.less';
+import './styles.scss';
 
 const App: React.FC = () => <h1>App React </h1>;
 export default App;
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,4 +1,29 @@
+const glob = require('glob');
+const PurgeCSSPlugin = require('purgecss-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const PATHS = {
+  src: path.join(__dirname, '..', 'src'),
+};
+
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss|less)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+    }),
+  ],
 };
